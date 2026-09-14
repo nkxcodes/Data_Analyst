12457099c9b3e871a964edb270e3768ba70f64e8
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-14 13:10:29 +0530

    Add initial project structure with .gitignore, README, requirements, and example Python scripts

---
+++ python/variables/001.py
@@ -0,0 +1,9 @@
+# Q1: Create three variable to store your name, age, and country. Print all three variables.
+
+name = 'Nitesh'
+age = '17'
+country = 'India'
+
+print(name)
+print(age)
+print(country)
---
+++ python/variables/002.py
@@ -0,0 +1,8 @@
+# Q2: Create variables to store two numbers. Add the two variables togerther and print the result.
+
+num_1 = 10
+num_2 = 13
+
+addition = num_1 + num_2
+
+print(addition)
