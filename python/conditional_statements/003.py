62a69623c04fa00a66fd0e458433040781ae0c1c
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-14 19:39:28 +0530

    Add script to evaluate user-input marks and determine pass or fail status

---
+++ python/conditional_statements/003.py
@@ -0,0 +1,16 @@
+"""
+Q3.​
+Create a variable called marks.
+If the marks are 50 or more, print:
+Pass
+Otherwise, print:
+Fail
+Test your program with marks below and above 50.
+"""
+
+marks = float(input('Enter your marks: '))
+
+if marks >= 50:
+    print('Pass')
+else:
+    print('Fail')
