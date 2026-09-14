77d65ef1bf0f349fc5ca8cbb7438c0ffd16c8e5a
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-14 13:32:46 +0530

    Add script to calculate final price using variables for item price, quantity, and discount

---
+++ python/variables/006.py
@@ -0,0 +1,15 @@
+"""
+Q6: Create variable for:
+        price of one item
+        number of items
+        dicount_amount
+    Use these variable to calculate the final price.
+"""
+
+price = 225
+number_of_items = 5
+discount_amount = 50
+
+final_price = (price * number_of_items) - discount_amount
+
+print(final_price)
