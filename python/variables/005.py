2b6ee3750e177f29201fff20d284bdc3085e1e72
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-14 13:29:36 +0530

    Add script to demonstrate variable reassignment and printing in Python

---
+++ python/variables/005.py
@@ -0,0 +1,14 @@
+"""
+Q5: Create a variable called score with an initial value of 50.
+    then change its value to 75, and finally change it to 100.
+    print the variable after each change.
+"""
+
+score = 50
+print(score)
+
+score = 75
+print(score)
+
+score = 100
+print(score)
