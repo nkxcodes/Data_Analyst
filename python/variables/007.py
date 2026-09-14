8f45a6ab789a48f3f1e8e3222339cebf3c839e14
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-14 16:12:43 +0530

    Add script to store student information and calculate total and average marks

---
+++ python/variables/007.py
@@ -0,0 +1,21 @@
+"""
+Q7: Create a simple program for a student.
+    Store the students's name and marks in three subjects
+    in a variables. Calculate and display:
+        student's name
+        total marks
+        average marks
+"""
+
+student_name = 'Manav'
+subject_1 = 98
+subject_2 = 78
+subject_3 = 67
+
+total_marks = subject_1 + subject_2 + subject_3
+
+average_marks = total_marks / 3
+
+print(student_name)
+print(total_marks)
+print(average_marks)
