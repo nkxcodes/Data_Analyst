89a8bcb10b8f6969f3bc6d50d0baa3ba4e338fbe
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-14 19:46:45 +0530

    Add script to compare two numbers and display their relationship

---
+++ python/conditional_statements/005.py
@@ -0,0 +1,18 @@
+"""
+Q5.​
+Create a program that compares two numbers stored in variables.
+Display whether:
+●​ the first number is greater than the second,
+●​ the first number is smaller than the second,
+●​ or both numbers are equal.
+"""
+
+num_1 = 80
+num_2 = 45
+
+if num_1 > num_2:
+    print('The first number is greater than second.')
+elif num_1 < num_2:
+    print('The first number is smaller than second.')
+else:
+    print('Both numbers are equal.')
