0082565f61fd26a6fdf8d616b58cece3bf22fe03
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-14 16:17:42 +0530

    Add script to calculate total cost of identical products using price and quantity variables

---
+++ python/variables/008.py
@@ -0,0 +1,18 @@
+"""
+Q8: A shop wants to calculate the cost of buying several
+    identical products.
+    Create variables for the price of one product and the
+    quantity puchases.
+    Calculate the total cost.
+"""
+
+price = 899
+quantity_purchased = 2
+
+total_cost = price * quantity_purchased
+print(total_cost)
+
+quantity_purchased = 5
+
+total_cost = price * quantity_purchased
+print(total_cost)
