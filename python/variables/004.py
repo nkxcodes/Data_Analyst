1d684248405fe014b54766d20b32e37b53af0dc3
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-14 13:26:52 +0530

    Add script to create and print variables of different data types: integer, decimal, string, and boolean

---
+++ python/variables/004.py
@@ -0,0 +1,27 @@
+"""
+Q4: Create variables containing: 
+    an integer
+    a decimal number
+    a string
+    a boolean
+
+    Print each variable and use Python to determine
+    the data type of each one.
+"""
+
+integer = 80
+decimal = 8.9
+string = 'Programming'
+boolean = True
+
+print(integer)
+print(type(integer))
+
+print(decimal)
+print(type(decimal))
+
+print(string)
+print(type(string))
+
+print(boolean)
+print(type(boolean))
