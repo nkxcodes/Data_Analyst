5dd2ea53f7590cf3e844d4723eb128544e40dd30
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-14 16:25:05 +0530

    Add script to demonstrate variable naming conventions and corrections in Python

---
+++ python/variables/111.py
@@ -0,0 +1,17 @@
+"""
+Q11.​
+Find and fix the mistake in this program:
+student_name = "Nitesh"
+student age = 18
+print(student_name)
+print(student age)
+After fixing it, think about why Python does not allow the original variable name.
+"""
+
+student_name = 'Karan'
+student_age =  18
+
+print(student_name)
+print(student_age)
+
+# student age - Python takes this as two seprate things not one variable name.
