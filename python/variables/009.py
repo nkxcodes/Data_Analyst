cbfe2d4f0446ee4110d435efaaa85be977c9f478
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-14 16:20:11 +0530

    Add script to prompt user for name and age, displaying a formatted message

---
+++ python/variables/009.py
@@ -0,0 +1,10 @@
+"""
+Q9: Create a small program that asks the user for their 
+    name and age. Store the answers in variables and display
+    a sentence such as: Manav is 18 years old
+"""
+
+name = input('Enter your name: ')
+age = int(input('Enter your age: '))
+
+print(f'{name} is {age} years old')
