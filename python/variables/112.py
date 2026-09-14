047023900acc4ba395bfe4c9d2139ab4fe9157a7
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-14 16:26:33 +0530

    Add script to demonstrate variable manipulation and output in Python

---
+++ python/variables/112.py
@@ -0,0 +1,18 @@
+"""
+Q12.​
+What will the following program print?
+x=5
+x = x + 10
+x=x*2
+x=x-5
+print(x)
+answer before running it.
+Then explain what happens to the value stored in x after each line.
+"""
+
+x = 5
+x = x + 10
+x = x * 2
+x = x - 5
+
+print(x)
