2b23961200547f069427c697b58e60ffc3ea128a
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-14 19:35:57 +0530

    Add script to check if a user is an adult based on age input

---
+++ python/conditional_statements/001.py
@@ -0,0 +1,10 @@
+"""
+Q1: Create a variable called age. if the age is 18 or above,
+    display: You are an adult.
+    Otherwise, display nothing
+"""
+
+age = int(input('Enter your age: '))
+
+if age >= 18:
+    print('You are an adult.')
