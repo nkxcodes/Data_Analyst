ff29cbc36fa5644bbd3479c1332ff0fa99fc7c0c
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-14 13:22:40 +0530

    Add age variable example script with age and next_age calculations

---
+++ python/variables/003.py
@@ -0,0 +1,12 @@
+"""
+Q3: Create a variable called age and stoe your current age in it
+    then create another variable called next_age that represents
+    your age next year. Print both values
+"""
+
+age = 16
+
+next_age = age + 1
+
+print(age)
+print(next_age)
