d818acbe2d9691b54973661e83db9f69c4d0277a
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-14 16:22:53 +0530

    Add script to demonstrate variable assignment and value retention in Python

---
+++ python/variables/110.py
@@ -0,0 +1,18 @@
+"""
+Q10.​
+Without running the program first, exactly what it will print:
+x = 10
+y=x
+x = 20
+print(x)
+print(y)
+Then run it.
+Explain why x and y do not contain the same value at the end.
+"""
+
+x = 10
+y = x
+x = 20
+
+print(x)
+print(y) # y is not connected to x, y is only received it's value
