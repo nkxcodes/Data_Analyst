a2a2a3f8c76b2b53617137fdbbc9f43e00c54d6f
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-15 14:36:09 +0530

    Add script to create a list of names and print each name using a loop

---
+++ python/loops/003.py
@@ -0,0 +1,12 @@
+"""
+Q3.
+Create a list of five names.
+
+Use a loop to print each name from the list one by one.
+"""
+
+
+names = ['Manav', 'Roy', 'Jay', 'Abhayankar', 'Susha']
+
+for name in names:
+    print(name)
