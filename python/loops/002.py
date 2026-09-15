fecee8a75bdfc985a9bee29c6ec8390d774f7ede
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-15 14:34:44 +0530

    Add script to create a loop that prints the word "Python" five times, demonstrating the efficiency of loops over repetitive print statements.

---
+++ python/loops/002.py
@@ -0,0 +1,9 @@
+"""
+Q2.
+Create a loop that prints the word Python 5 times.
+
+After writing it, think about why using a loop is better here than writing print("Python") five separate times.
+"""
+
+for i in range(1, 6):
+    print('Python')
