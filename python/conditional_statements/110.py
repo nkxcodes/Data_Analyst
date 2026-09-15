fcaf70f9c8ec3a88d0cbd20669b10ab90a34f69a
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-15 14:05:16 +0530

    Add script to demonstrate conditional statements with nested if-else logic

---
+++ python/conditional_statements/110.py
@@ -0,0 +1,21 @@
+"""
+Q10.​
+Before running the following program, predict exactly what it will print:
+x = 10
+if x > 5:
+print("A")
+if x > 8:
+print("B")
+else:
+print("C")
+Then run it and explain which else belongs to which if.
+"""
+
+x = 10
+
+if x > 5:
+    print("A")
+if x > 8:
+    print("B")
+else:
+    print("C")
