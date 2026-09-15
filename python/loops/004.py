b17a2d10287e315cd476afaa6d564b055348750c
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-15 14:37:00 +0530

    Add script to print numbers from 1 to 20, and modify it to print only numbers from 5 to 15 using a loop

---
+++ python/loops/004.py
@@ -0,0 +1,9 @@
+"""
+Q4.
+Use range() and a loop to print all the numbers from 1 to 20.
+
+Then modify your program so that it prints only the numbers from 5 to 15.
+"""
+
+for num in range(1, 21):
+    print(num)
