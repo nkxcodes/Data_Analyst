bdfaf6466a0c67f8d4339348360bd07a4fc9a6b3
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-15 18:39:27 +0530

    Add script to create and print a list of five programming languages, demonstrating basic list operations.

---
+++ python/list/001.py
@@ -0,0 +1,9 @@
+"""
+Q1.
+Create a list containing the names of five programming languages.
+Print the entire list.
+"""
+
+programming_language = ['Python', 'Java', 'Javascript', 'Golang', 'CSS']
+
+print(programming_language)
