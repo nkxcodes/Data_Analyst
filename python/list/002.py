909ebdfc61f4db2ce1130d1e5d5287a7a0cb530b
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-15 18:42:26 +0530

    Add script to create a list of five numbers and print the first, third, and last numbers, demonstrating basic list indexing.

---
+++ python/list/002.py
@@ -0,0 +1,14 @@
+"""
+Q2.
+Create a list containing five numbers.
+Print:
+● the first number
+● the third number
+● the last number
+"""
+
+numbers = [32, 45, 65, 76, 87]
+
+print(numbers[0]) # First number
+print(numbers[2]) # Third number
+print(numbers[-1]) # Last number
