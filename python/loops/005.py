e3504a63df9b5186ded7ca418fa2088aa6a08ab8
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-15 14:39:58 +0530

    Add script to print the multiplication table of a specified number from 5 to 50 using a loop.

--- python/loops/005.py
+++ python/loops/005.py
@@ -5,3 +5,7 @@ Create a program that prints the multiplication table of a number chosen by you.
 For example, if the number is 5, the program should print its table from 5 × 1 through 5 × 10.
 """
 
+number = 5
+
+for num in range(number, (number * 10) + 1, number):
+    print(num)
