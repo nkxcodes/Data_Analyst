3191909296510be1f82f5429e154b216c0684029
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-15 18:44:47 +0530

    Add script to create a list of numbers from 1 to 10 and print the second, fifth, and eighth elements, demonstrating list indexing.

---
+++ python/list/003.py
@@ -0,0 +1,11 @@
+"""
+Q3.
+Create a list containing the numbers from 1 to 10.
+Use indexing to print the second, fifth, and eighth elements.
+"""
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print(numbers[1]) # Second element
+print(numbers[4]) # Fifth element
+print(numbers[7]) # Eighth element
