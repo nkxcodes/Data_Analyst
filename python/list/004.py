d71ebed36a5848a63523aa494f5e461d33548293
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-15 18:46:21 +0530

    Add script to create a list of 10 numbers and demonstrate slicing by printing the first 5 elements, last 3 elements, and elements from the 3rd to the 7th position.

---
+++ python/list/004.py
@@ -0,0 +1,14 @@
+"""
+Q4.
+Create a list containing 10 numbers.
+Use slicing to create and print:
+● the first 5 elements
+● the last 3 elements
+● the elements from the 3rd through the 7th position
+"""
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print(numbers[:5])
+print(numbers[:3])
+print(numbers[3:7])
