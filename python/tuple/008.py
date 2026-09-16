e07f675d16d2033d0668f64b379f8f238980f170
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-16 14:02:28 +0530

    Add script to create two point tuples and calculate the differences in their x and y coordinates, demonstrating tuple operations in Python.

---
+++ python/tuple/008.py
@@ -0,0 +1,17 @@
+"""
+Q8.​
+A point on a graph can be represented using a tuple containing two numbers:
+(x, y)
+Create two point tuples.
+Then calculate the difference between their x-coordinates and the difference between their
+y-coordinates.
+"""
+
+point_1 = (5, 8)
+point_2 = (2, 3)
+
+difference_in_x = point_1[0] - point_2[0]
+difference_in_y = point_1[1] - point_2[1]
+
+print(difference_in_x)
+print(difference_in_y)
