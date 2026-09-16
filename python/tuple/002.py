4a1bd4ddc2fce03708034d707731b07a4918d3c4
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-16 12:40:15 +0530

    Add script to create a tuple of five numbers and print the first, third, and last elements, demonstrating basic tuple operations in Python.

---
+++ python/tuple/002.py
@@ -0,0 +1,14 @@
+"""
+Q2.​
+Create a tuple containing five numbers.
+Print:
+●​ the first element
+●​ the third element
+●​ the last element
+"""
+
+numbers = (23, 43, 45, 65, 76)
+
+print(numbers[0])
+print(numbers[2])
+print(numbers[-1])
