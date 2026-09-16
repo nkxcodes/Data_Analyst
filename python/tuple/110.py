94c548ba9026c5405d48051fff09f2f7839ff95d
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-16 14:07:01 +0530

    Add script to demonstrate tuple immutability by attempting to modify an element in a tuple and explaining the resulting behavior in Python.

---
+++ python/tuple/110.py
@@ -0,0 +1,15 @@
+"""
+Q10.​
+Predict what will happen when this program runs:
+numbers = (10, 20, 30)
+numbers[1] = 50
+print(numbers)
+Will Python successfully change the tuple?
+Run the program afterward and explain why Python behaves this way.
+"""
+
+numbers =(10, 20, 30)
+
+numbers[1] = 50
+
+print(numbers) # Tuple is immutable after creation
