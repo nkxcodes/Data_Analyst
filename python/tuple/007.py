05c9208ee3f2012dddf190d3ce62dd1de43edbf5
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-16 13:58:35 +0530

    Add script to create a tuple representing student details and display the information in a readable format, demonstrating tuple usage in Python.

---
+++ python/tuple/007.py
@@ -0,0 +1,21 @@
+"""
+Q7.​
+Imagine a student record is stored as a tuple containing:
+●​
+●​
+●​
+●​
+student name
+age
+class
+country
+Access each piece of information individually and display it as a readable student profile.
+"""
+
+students_details = ('Manav', 17, '12th', 'India')
+
+print('Student Details: ')
+print(f'Name: {students_details[0]}')
+print(f'Age: {students_details[1]}')
+print(f'Class: {students_details[2]}')
+print(f'Country: {students_details[3]}')
