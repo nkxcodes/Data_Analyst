9ff92b78a00594ab3162571d104a0258f9d1935a
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-16 13:54:41 +0530

    Add script to create a tuple of five numbers and check for the existence of specific numbers, demonstrating membership testing in Python.

---
+++ python/tuple/006.py
@@ -0,0 +1,12 @@
+"""
+Q6.​
+Create a tuple of five numbers.
+Check whether a particular number exists in the tuple.
+Then check for a number that does not exist.
+Your program should clearly tell you whether each number is present.
+"""
+
+numbers = (54, 43, 76, 87, 45)
+
+print(54 in numbers)
+print(102 in numbers)
