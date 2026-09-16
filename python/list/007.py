ee99eabfc87d274c67200086c71df4b4565bddc9
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-16 12:04:28 +0530

    Add script to track and calculate the total number of Python problems solved over several days using a list.

---
+++ python/list/007.py
@@ -0,0 +1,14 @@
+"""
+Q7.​
+You are keeping track of your Python problems solved each day.
+Create a list containing the number of problems solved over several days.
+Then calculate the total number of problems solved.
+Think about how the list helps you store multiple related values instead of creating a separate
+variable for every day.
+"""
+
+python_problems = [12, 12, 7, 32, 21, 32, 34, 45, 12, 11]
+
+total_problems = len(python_problems)
+
+print(total_problems)
