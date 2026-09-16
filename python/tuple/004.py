33d509d19b4ad845171e48c19b1538ce2f0d2909
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-16 13:49:43 +0530

    Add script to create a tuple containing numbers from 1 to 10 and demonstrate slicing by printing the first five numbers, the last three numbers, and the numbers from position 3 to position 7.

---
+++ python/tuple/004.py
@@ -0,0 +1,14 @@
+"""
+Q4.​
+Create a tuple containing numbers from 1 to 10.
+Use slicing to create and print:
+●​ the first five numbers
+●​ the last three numbers
+●​ the numbers from position 3 to position 7
+"""
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print(numbers[:5])
+print(numbers[:-4:-1])
+print(numbers[3:7])
