034207b1d15d5fb5e4f22ae9632d8f7795d61a84
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-16 12:41:45 +0530

    Add script to create a tuple of five programming languages and print the second and fourth languages using indexing, demonstrating tuple indexing in Python.

---
+++ python/tuple/003.py
@@ -0,0 +1,10 @@
+"""
+Q3.​
+Create a tuple containing the names of five programming languages.
+Use indexing to print the name of the second and fourth languages.
+"""
+
+programming_languages = ('Python', 'Java', 'Golang', 'HTML', 'CSS')
+
+print(programming_languages[1])
+print(programming_languages[3])
