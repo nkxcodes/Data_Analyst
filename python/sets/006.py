4afccac2e49a43a12983a540f9b7b60f2d35f27f
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-17 14:52:16 +0530

    Add functionality to demonstrate set difference operation in addition to union, enhancing the script for set operations in Python.

--- python/sets/006.py
+++ python/sets/006.py
@@ -13,3 +13,7 @@ set_b = {4, 5, 6, 7, 8}
 # ●​ all elements that belong to either set - (|)(union) - combine both sets
 print(set_a.union(set_b))
 print(set_a | set_b)
+
+# ●​ elements that belong to Set A but not Set B - (-)(difference) - Start with A → remove B's elements.
+print(set_a.difference(set_b))
+print(set_a - set_b)
