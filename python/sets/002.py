198d552265c67b4765177cd112697890aacd8f0b
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-17 14:39:19 +0530

    Add script to create a set of numbers, check for the existence of specific values, and demonstrate set membership in Python.

---
+++ python/sets/002.py
@@ -0,0 +1,12 @@
+"""
+Q2.​
+Create a set containing these numbers:
+10, 20, 30, 40, 50
+Check whether 30 exists in the set.
+Then check whether 100 exists.
+"""
+
+numbers = {10, 20, 30, 40, 50}
+
+print(30 in numbers)
+print(100 in numbers)
