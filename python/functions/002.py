18ea31151e0fa032f43f1df92dc3f126842e7993
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-17 20:59:30 +0530

    Add new file containing a function `show_name()` that prints a person's name. The function is called with three different names to demonstrate its usage, providing a simple example of function creation and execution in Python.

---
+++ python/functions/002.py
@@ -0,0 +1,11 @@
+"""
+Q2.
+Create a function called show_name() that takes a person's name as an argument and prints:
+My name is <name>
+Call it with at least three different names.
+"""
+
+def show_name(name):
+    print(f'My name is {name}')
+
+show_name('Rahul')
