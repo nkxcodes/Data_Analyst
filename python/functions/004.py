b5cb09188db7e47be7eeebed4d504443a753b462
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-17 21:03:58 +0530

    Add new file containing a function `add()` that takes two numbers as parameters and returns their sum. The function is tested with an example, and an explanation of the difference between parameters and arguments is provided, enhancing understanding of function usage in Python.

---
+++ python/functions/004.py
@@ -0,0 +1,17 @@
+"""
+Q4.
+Create a function called add() that takes two numbers and returns their sum.
+Test it with different pairs of numbers.
+Then explain the difference between the parameters in the function definition and the
+arguments you provide when calling it.
+"""
+
+def add(a, b):
+    return a + b
+
+result = add(5, 4)
+
+print(result)
+
+# Parameter - the variable written in the function definition.
+# Argument - the actual value you give when calling the function.
