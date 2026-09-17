c572df29a59999b7e83b9af9090c9f772c0c5dbb
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-17 16:04:10 +0530

    Add new file containing a dictionary example that includes personal information such as name, age, and country. This resource serves as a practical exercise to demonstrate dictionary creation and usage in Python.

---
+++ python/dictionaries/001.py
@@ -0,0 +1,16 @@
+"""
+Q1.​
+Create a dictionary containing information about yourself:
+●​ name
+●​ age
+●​ country
+Print the entire dictionary.
+"""
+
+details = {
+    "name": "Nitesh",
+    "age": "17",
+    "country": "India"
+}
+
+print(details)
