434802a087fa6ff914aef34d4aa136c69a3a102a
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-17 21:00:48 +0530

    Add new file containing a function `square()` that calculates the square of a given number. The function is called with the number 2, and the result is printed, providing a simple example of function creation and execution in Python.

---
+++ python/functions/003.py
@@ -0,0 +1,12 @@
+"""
+Q3.
+Create a function called square() that takes a number and returns its square.
+Call the function with different numbers and print the returned values.
+"""
+
+def square(number):
+    return number ** number
+
+result = square(2)
+
+print(result)
