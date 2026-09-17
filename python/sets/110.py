e979ef959b7a33653cfc810eea7d201f793cef6c
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-17 15:09:27 +0530

    Add new script to demonstrate the limitations of indexing in sets, including an explanation of why sets cannot be accessed or modified using indices like lists.

---
+++ python/sets/110.py
@@ -0,0 +1,17 @@
+"""
+Q10.​
+Predict what happens before running this:
+numbers = {10, 20, 30}
+numbers[0] = 100
+print(numbers)
+Run it afterward.
+Explain why you cannot access or change a set using an index like you can with a list.
+"""
+
+numbers = {10, 20, 30}
+numbers[0] = 100 
+
+print(numbers)
+
+# Sets are unordered + no indexing.
+# Sets don't have positions/indexes like lists do.
