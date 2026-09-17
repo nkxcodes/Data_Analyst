b8763912dade5cfa72dbbc4f46c5b227a3786218
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-17 14:38:15 +0530

    Add script to create a set of five programming languages and print the set, demonstrating set creation and unordered nature in Python.

---
+++ python/sets/001.py
@@ -0,0 +1,11 @@
+"""
+Q1.​
+Create a set containing five different programming languages.
+Print the entire set.
+Then observe whether Python displays the elements in the same order in which you wrote
+them.
+"""
+
+programming_languages = {'Python', 'Java', 'Javascript', 'HTML', 'CSS'}
+
+print(programming_languages)
