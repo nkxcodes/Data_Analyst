d606ccd9dcb20df3b3bb0657fb40b674197b0e79
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-19 21:02:40 +0530

    Add new file demonstrating the import of specific functions from the math module to find the largest integer less than or equal to a number and the smallest integer greater than or equal to a number. Includes a brief explanation of the task with test cases.

---
+++ python/import_modules_and_packages/005.py
@@ -0,0 +1,14 @@
+"""
+Q5. — Medium
+From the math module, import two specific functions that can help you:
+●​ find the largest integer less than or equal to a number
+●​ find the smallest integer greater than or equal to a number
+Test them with:
+7.8
+and
+-3.2
+"""
+from math import floor, ceil
+
+print(floor(7.8))
+print(ceil(-3.2))
