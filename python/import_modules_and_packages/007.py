285858a8a69f475d919a9bb705b6e9d366e4bd2b
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-19 21:07:38 +0530

    Add new file implementing a simple dice game that generates a random number between 1 and 6 using the random module. Includes a brief description of the task.

---
+++ python/import_modules_and_packages/007.py
@@ -0,0 +1,10 @@
+"""
+Q7. — Application
+You are creating a simple dice game.
+The program needs to generate a random number from 1 to 6 whenever the user rolls the dice.
+Choose an appropriate Python module and create the small program.
+"""
+
+from random import randint
+
+print(randint(1, 6))
