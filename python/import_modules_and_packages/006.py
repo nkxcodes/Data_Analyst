d8e38d23891de656ba8bf46189e3c1aca73178ac
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-19 21:05:55 +0530

    Add new file explaining the differences between importing the entire math module and specific functions. Includes answers to questions about accessing functions and code readability.

---
+++ python/import_modules_and_packages/006.py
@@ -0,0 +1,15 @@
+"""
+Q6. — Medium
+Consider these two import styles:
+import math
+and
+from math import sqrt
+Without running them, explain:
+1.​ What name would you use to access sqrt in each case?
+2.​ Which approach gives you access to the whole math module?
+3.​ Why might importing only what you need sometimes make the code easier to read?
+"""
+
+# Ans1 :- math.sqrt(), sqrt()
+# Ans2 :- import math
+# Ans3 :- because we don't need full math module in that case, so, people can understand okay we using only this function from this module.
