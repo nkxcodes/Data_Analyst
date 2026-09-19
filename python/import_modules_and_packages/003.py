88af28b019fd41c420975f4d6cabdc62e4977246
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-19 20:58:24 +0530

    Add new file demonstrating the import of the sqrt function from the math module to calculate the square root of 144. Includes a brief explanation of the task and highlights the difference from importing the entire module.

---
+++ python/import_modules_and_packages/003.py
@@ -0,0 +1,11 @@
+"""
+Q3. — Easy
+Import only the sqrt function from the math module.
+Use it to find the square root of:
+144
+Think about how this is different from importing the entire math module.
+"""
+
+from math import sqrt
+
+print(sqrt(144))
