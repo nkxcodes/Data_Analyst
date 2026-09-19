5f6a1d9e097ddeb126ad1acf866ec87b7b812509
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-19 21:11:38 +0530

    Add new file implementing a simple calculator program that utilizes the math module to perform square roots, powers, absolute values, and floor values. Includes a brief description of the task.

---
+++ python/import_modules_and_packages/008.py
@@ -0,0 +1,17 @@
+"""
+Q8. — Application
+You are making a small calculator program.
+The program needs to calculate:
+square roots
+powers
+absolute values
+floor values
+Instead of writing these operations yourself, use an appropriate built-in Python module.
+"""
+
+import math
+
+print(math.sqrt(25))
+print(math.pow(2, 3))
+print(math.fabs(-7))
+print(math.floor(9.8))
