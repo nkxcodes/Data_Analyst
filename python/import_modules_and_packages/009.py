85ba1405977e27469d173d281a18d60c5b197672
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-19 21:13:13 +0530

    Add new file implementing a program that displays the current date and time using the datetime module. Includes a brief description of the task.

---
+++ python/import_modules_and_packages/009.py
@@ -0,0 +1,10 @@
+"""
+Q9. — Application
+You are creating a program that displays the current date and time.
+Find an appropriate Python module and use it to display the current date and time.
+You don't need to format it in any special way.
+"""
+
+import datetime
+
+print(datetime.datetime.now())
