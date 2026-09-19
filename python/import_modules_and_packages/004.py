e764cd62e42e3b282f84c9abcad502cfbbd5013e
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-19 20:59:59 +0530

    Add new file demonstrating the import of the math module using an alias to calculate the value of π and the square root of 100. Includes a brief explanation of the task.

---
+++ python/import_modules_and_packages/004.py
@@ -0,0 +1,12 @@
+"""
+Q4. — Medium
+Import the math module using an alias.
+Then use the alias to calculate:
+●​ the value of π
+●​ the square root of 100
+"""
+
+import math as m
+
+print(m.pi)
+print(m.sqrt(100))
