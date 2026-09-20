ecb3c1e123e020e9b206aab024970cc67de7fa6f
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-20 16:09:14 +0530

    Add new file 003.py that imports timedelta from the datetime module to create and print a timedelta representing 5 days, 3 hours, and 30 minutes. Includes a brief description of the program's functionality.

---
+++ python/standard_library_overview/003.py
@@ -0,0 +1,13 @@
+"""
+Q3. — Easy
+Import timedelta from datetime.
+Create a timedelta representing:
+5 days, 3 hours, and 30 minutes
+Then print it.
+"""
+
+from datetime import timedelta
+
+time = timedelta(days=5, hours=3, minutes=30)
+
+print(time)
