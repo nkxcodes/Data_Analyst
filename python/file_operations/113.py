559224ac4601aada077395d6d6c4d5a9fe724253
Nitesh <niteshkjaiswal108@gmail.com>
2026-09-25 18:24:47 +0530

    Add new files 113.py, data.txt, marks.txt, story.txt, and students.txt. The 113.py file outlines a program to read numbers from numbers.txt, filter even numbers, and write them to even_numbers.txt. The data.txt contains the string "Python", marks.txt includes a list of marks, story.txt features brief statements about Python and Linux, and students.txt lists student names.

---
+++ python/file_operations/113.py
@@ -0,0 +1,12 @@
+"""
+Q13. — Mixed
+You have:
+numbers.txt
+containing one number per line.
+Create a program that:
+1.​ Reads all the numbers.
+2.​ Converts them into integers.
+3.​ Finds only the even numbers.
+4.​ Writes those even numbers into a new file called even_numbers.txt.
+Use file operations, lists, loops, and conditions.
+"""
